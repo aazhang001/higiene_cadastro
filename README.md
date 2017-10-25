47aa6860b61a26f096ef2971b859ceb746ef0acd
devplusti <elton@devplus.com.br>
2017-10-25 17:09:15 -0200

    incluindo readme e programa para teste das rotinas

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# higiene_cadastro
+Rotinas para limpar cadastros, ou seja, tornar descrições mais legiveis, remover erros (TODO), etc
+
+## Installation
+TODO: Describe the installation process
+
+## Usage
+
+```
+import base
+
+pecas = ['001022','000119']
+
+for i in pecas:
+    descricao = base.pegar_descricao(i)
+    t = descricao[0]
+    print('CODIGO DA PEÇA...:',i)
+    print('DESCRIÇÃO ATUAL..:',t[0])
+
+    corrigido = base.verifica_ortografia(t[0])
+    trocada = base.verifica_descricao(corrigido)
+
+    if trocada != corrigido:
+        corrigido = trocada
+
+    print('DESCRIÇÃO CORRETA:',corrigido)
+    print()
+```
+
+## SAÍDA DOS COMANDOS
+
+```
+CODIGO DA PEÇA...: 001022
+DESCRIÇÃO ATUAL..: CRUZETA COLUNA DIRECAO
+DESCRIÇÃO CORRETA: CRUZETA COLUNA DIREÇÃO
+
+CODIGO DA PEÇA...: 000119
+DESCRIÇÃO ATUAL..: JUNTA TAMPA CX. DIRECAO
+DESCRIÇÃO CORRETA: JUNTA TAMPA CAIXA DIREÇÃO
+```
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+## History
+TODO: Write history
+## Credits
+TODO: Write credits
+## License
+TODO: Write license
--- base.py
+++ base.py
@@ -1,31 +1,38 @@
 import pymysql
 import enchant
 
-def conecta_banco()
+def conecta_banco():
     return pymysql.connect(host='srvhomolog.cipec.net', port=3306, user='root', passwd='beleza', db='homocipec')
 
 
-def gera_base_dados()
+def pegar_descricao(codigo=''):
     conn = conecta_banco()
     conn.show_warnings()
     cur = conn.cursor()
 
-    cur.execute(
-        "select pecas.descricao, pecas.codigo from pecas where pecas.sr_deleted != 'T' and codigo > 023600")
-    pecas = cur.fetchall()
+    if codigo is None:
+        sql = "select pecas.descricao, pecas.codigo from pecas where pecas.sr_deleted != 'T'"
+    else:
+        sql = "select pecas.descricao, pecas.codigo from pecas where pecas.sr_deleted != 'T' and codigo = '"+codigo+"' "
 
+    cur.execute(sql)
+    return cur.fetchall()
+
+
+def gerar_base_dados(codigo=''):
+    conn = conecta_banco()
+    conn.show_warnings()
+    cur = conn.cursor()
+
+    pecas = pegar_descricao(codigo)
     for peca in pecas:
         if peca[0] == None:
             continue  # volta para o for
 
         palavras = peca[0].split()
-        # print(peca[1])
-
         for palavra in palavras:
             palavra = palavra.replace('"', '')
             palavra = palavra.replace("'", "")
-
-            # print('\t',palavra)
 
             sql = "select incidencia from palavras where palavras.origem=%s"
             cur.execute(sql, palavra)
@@ -33,12 +40,9 @@ def gera_base_dados()
 
             if result == None:
                 incidencia = 0
-            else:
-                incidencia = int(result[0])
-
-            if incidencia == 0:
                 sql = "INSERT INTO palavras (`incidencia`, `origem`, `sr_deleted`) VALUES (%s, '%s', '')"
             else:
+                incidencia = int(result[0])
                 sql = "UPDATE palavras SET `incidencia` = %s WHERE `origem` = '%s'"
 
             incidencia = incidencia + 1
@@ -47,20 +51,19 @@ def gera_base_dados()
             conn.commit()
 
 
-def trocar_palavra(descricao)
+def verifica_descricao(descricao):
     conn = conecta_banco()
     cur = conn.cursor()
 
     palavras = descricao.split()
-    nova = descricao
+    descricao_corrigida = descricao
 
     # TROCAS OBRIGATORIAS
-    nova = nova.replace("O'RING", "O-RING")
-    nova = nova.replace('"', 'POL ')
-    nova = nova.replace("'", "")
+    descricao_corrigida = descricao_corrigida.replace("O'RING", "O-RING")
+    descricao_corrigida = descricao_corrigida.replace('"', 'POL ')
+    descricao_corrigida = descricao_corrigida.replace("'", "")
 
     for origem in palavras:
-
         if len(origem) > 2:
             sql = "select destino from palavras where palavras.origem = %s and destino is not null and palavras.sr_deleted != 'T'"
             cur.execute(sql, origem)
@@ -68,20 +71,22 @@ def trocar_palavra(descricao)
 
             if troca != None:
                 destino = troca[0]
-                print(origem, "-", destino)
-                nova = nova.replace(origem, destino)
+                descricao_corrigida = descricao_corrigida.replace(origem, destino)
 
-    #print("ANTIGO[", descricao, "]", "NOVO[", nova, "]")
-
-    return (nova)
+    return (descricao_corrigida)
 
 
-def verifica_ortografia(descricao)
+def verifica_ortografia(descricao):
     d = enchant.Dict("pt_BR")
+    descricao_ortografia_corrigida = ''
 
     for palavra in descricao.split():
         if d.check(palavra):
-            if d.check(palavra)
+           descricao_ortografia_corrigida = descricao_ortografia_corrigida + ' ' + palavra
+        else:
+           #selecionar qual a palavra mais correta
+           #print(d.suggest(palavra))
+           t = d.suggest(palavra)
+           descricao_ortografia_corrigida = descricao_ortografia_corrigida + ' ' + t[0]
 
-            
-    return (descricao_corrigida)
+    return (descricao_ortografia_corrigida.strip())
